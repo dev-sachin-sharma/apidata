6dfa3d2d1b1b6974df56ce3e6a0e4897741ab217
sachin <sharmascahin066@gmail.com>
2021-06-01 18:57:43 +0530

    updated

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -11,19 +11,19 @@ import styles from './Post.module.css'
      }
      
      componentDidMount(){
-         fetch('http://jsonplaceholder.typicode.com/posts')
+         fetch('https://my-json-server.typicode.com/typicode/demo/posts')
          .then(res => res.json())
          .then(data => this.setState({posts:data}))
      }
      handleTruncate=(str,n)=>{
-        const title = str && str.length > 10 ? str.substr(0,) 
+        return str && str.length > n ? str.substr(0,n) + "..." : str
      }
 
     render() {
         const postItem=this.state.posts && this.state.posts.map((data)=>(
             <div className={styles.postCard} key={data.id}>
                 <h3>{this.handleTruncate(data.title,10)}</h3>
-                <p>{data.body}</p>
+                
             </div>
         ))
         return (
